51cb9d26f4026972cc97b3332d7f62614b7d56e0
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-23 03:20:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,8 +10,6 @@ def nyc_pigeon_organizer(data)
   end
   stathash = Hash[attributes.map{|stat| [stat, []]}]
   birbstats = birblist.map{|k,v| v = stathash}
-  data.map{|stat,value| value.map{|k,name| name.each{|x| birbstats[x.to_sym][stat] << k.to_s}}}
-  
-  # birbstats[name.to_sym][stat] << k.to_s}}
+  data.map{|stat,value| value.map{|key,names| names.each{|x| birbstats[x.to_sym][stat] << key.to_s}}}
   return birbstats
 end
