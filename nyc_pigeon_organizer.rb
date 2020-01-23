88e67b77398a4c5443c75c5016f12a1de1d95e59
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-23 04:24:50 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  attributeindex = 0
+  attributes = []
+  birblist = Hash.new
+  #this while loop populates a hash with all the names as keys pointing to empty hashes and accumulates all the attribute names (i.e. colors, gender, lives) in an array
+  while attributeindex < data.length do
+    data.values[attributeindex].map{|k,v| v.map{|birb| birblist[birb] = {}}}
+    attributes << data.keys[attributeindex]
+    attributeindex+=1
+  end
+   # stathash = Hash[attributes.map{|stat| [stat, []]}]
+   birbstats = birblist.map{|k,v| [k, v = Hash[attributes.map{|stat| [stat, []]}]]}.to_h
+   # pp 'ddsafsd'
+    #pp  data
+    #pp birbstats
+   # data.map{|stat,value| value.map{|key,names| names.each{|x| birbstats[x.to_sym][stat] << key.to_s}}}
+   data.map { |stat, value| 
+     value.map { |key,names|
+
+       names.map{ |x|
+        birbstats[x][stat] << key.to_s
+
+        }
+      }
+     }
+  
+   return birbstats
 end
