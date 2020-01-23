56f68d493485b9e487516706d679405c35e997f3
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-23 02:32:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,13 @@
 def nyc_pigeon_organizer(data)
   attributeindex = 0
+  attributes = []
   output = Hash.new
-  #this while loop populates a hash with all the names as keys pointing to nothing
+  #this while loop populates a hash with all the names as keys pointing to empty hashes and accumulates all the attribute names (i.e. colors, gender, lives) in an array
   while attributeindex < data.length do
     data.values[attributeindex].map{|k,v| v.map{|birb| output[birb] = {}}}
+    attributes << data.keys[attributeindex]
     attributeindex+=1
   end
-  
+  output.map{|birb,stats| 
   return output
 end
