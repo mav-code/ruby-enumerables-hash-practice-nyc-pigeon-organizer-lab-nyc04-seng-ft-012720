2abd01c4fb3dabb3dbb6c6c7337c7f8bb528b9a7
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-23 02:04:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,12 @@
 def nyc_pigeon_organizer(data)
   atrributeindex = 0
+  attributes =[]
   output = Hash.new
   while atrributeindex < data.length do
-    data[attributeindex].map{|
+    data[attributeindex].map{|k,v| attributes << k; v.map
     
   
-  
+    attributeindex+=1
   end
+  return output
 end
